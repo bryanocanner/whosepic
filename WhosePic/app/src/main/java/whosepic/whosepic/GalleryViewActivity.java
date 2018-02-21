f15c1cd970a5788827a33be2f9f4e7cf8acd60ed
Emin Tosun <tosunbilkent@gmail.com>
2018-02-21 19:59:08 +0300

    resim cekildi dummy olarak
    Summary:
    
    Ref
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:

--- WhosePic/app/src/main/java/whosepic/whosepic/GalleryViewActivity.java
+++ WhosePic/app/src/main/java/whosepic/whosepic/GalleryViewActivity.java
@@ -1,5 +1,6 @@
 package whosepic.whosepic;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.TypedArray;
@@ -8,8 +9,10 @@ import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.GridView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Random;
@@ -21,6 +24,7 @@ import java.util.Random;
 public class GalleryViewActivity extends AppCompatActivity {
     private GridView gridView;
     private GridViewAdapter gridAdapter;
+    private ArrayList<String> images;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,20 +32,64 @@ public class GalleryViewActivity extends AppCompatActivity {
         setContentView(R.layout.activity_gallery);
 
         gridView = (GridView) findViewById(R.id.gridView);
-        gridAdapter = new GridViewAdapter(this, R.layout.image_view, getData());
+        gridAdapter = new GridViewAdapter(this,images,this);
         gridView.setAdapter(gridAdapter);
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1,
+                                    int position, long arg3) {
+                if (null != images && !images.isEmpty())
+                    Toast.makeText(
+                            getApplicationContext(),
+                            "position " + position + " " + images.get(position),
+                            Toast.LENGTH_LONG).show();
+                ;
+
+            }
+        });
+
     }
 
     // Prepare some dummy data for gridview
-    private ArrayList<Bitmap> getData() {
-        final ArrayList<Bitmap> imageItems = new ArrayList<>();
-        //TypedArray imgs = getResources().obtainTypedArray(R.mipmap.ic_launcher);
-        int a = (int)(Math.random() * 15);
-        for (int i = 0; i < a; i++) {//imgs.length(); i++) {
-            //Bitmap bitmap = BitmapFactory.decodeResource(getResources(), imgs.getResourceId(i, -1));
-            Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher);;
-            imageItems.add(bitmap);
-        }
-        return imageItems;
-    }
+//    private ArrayList<Bitmap> getData() {
+//        final ArrayList<Bitmap> imageItems = new ArrayList<>();
+//        //TypedArray imgs = getResources().obtainTypedArray(R.mipmap.ic_launcher);
+//        int a = (int)(Math.random() * 15);
+//        for (int i = 0; i < a; i++) {//imgs.length(); i++) {
+//            //Bitmap bitmap = BitmapFactory.decodeResource(getResources(), imgs.getResourceId(i, -1));
+//            Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher);;
+//            imageItems.add(bitmap);
+//        }
+//        return imageItems;
+//    }
 }
+//public class GallarySample extends Activity {
+//
+//    /** The images. */
+//    private ArrayList<String> images;
+//    @Override
+//    public void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.gallery_activity);
+//
+//        GridView gallery = (GridView) findViewById(R.id.galleryGridView);
+//
+//        gallery.setAdapter(new ImageAdapter(this));
+//
+//        gallery.setOnItemClickListener(new OnItemClickListener() {
+//
+//            @Override
+//            public void onItemClick(AdapterView<?> arg0, View arg1,
+//                                    int position, long arg3) {
+//                if (null != images && !images.isEmpty())
+//                    Toast.makeText(
+//                            getApplicationContext(),
+//                            "position " + position + " " + images.get(position),
+//                            300).show();
+//                ;
+//
+//            }
+//        });
+//
+//    }
--- WhosePic/app/src/main/java/whosepic/whosepic/GridViewAdapter.java
+++ WhosePic/app/src/main/java/whosepic/whosepic/GridViewAdapter.java
@@ -2,55 +2,190 @@ package whosepic.whosepic;
 
 import android.app.Activity;
 import android.content.Context;
+import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
+import com.bumptech.glide.*;
 /**
  * Created by aligunes on 18/02/2018.
  */
 
-public class GridViewAdapter extends ArrayAdapter {
+public class GridViewAdapter extends BaseAdapter {
     private Context context;
     private int layoutResourceId;
     private ArrayList<Bitmap> data = new ArrayList<Bitmap>();
+    private ArrayList<String> images;
 
-    public GridViewAdapter(Context context, int layoutResourceId, ArrayList data) {
-        super(context, layoutResourceId, data);
-        this.layoutResourceId = layoutResourceId;
+    public GridViewAdapter(Context context, ArrayList<String> images,Activity activity) {
         this.context = context;
-        this.data = data;
+        this.images = images;
+        this.images = getAllShownImagesPath(activity);
     }
 
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        View row = convertView;
-        ViewHolder holder = null;
-
-        if (row == null) {
-            LayoutInflater inflater = ((Activity) context).getLayoutInflater();
-            row = inflater.inflate(layoutResourceId, parent, false);
-            holder = new ViewHolder();
-            holder.imageTitle = (TextView) row.findViewById(R.id.text);
-            holder.image = (ImageView) row.findViewById(R.id.image);
-            row.setTag(holder);
-        } else {
-            holder = (ViewHolder) row.getTag();
-        }
-
-        Bitmap item = data.get(position);
-        holder.image.setImageBitmap(item);
-        return row;
-    }
 
     static class ViewHolder {
         TextView imageTitle;
         ImageView image;
     }
+
+    public int getCount() {
+        return images.size();
+    }
+
+    public Object getItem(int position) {
+        return position;
+    }
+
+    public long getItemId(int position) {
+        return position;
+    }
+
+    public View getView(final int position, View convertView,
+                        ViewGroup parent) {
+        ImageView picturesView;
+        if (convertView == null) {
+            picturesView = new ImageView(context);
+            picturesView.setScaleType(ImageView.ScaleType.FIT_CENTER);
+            picturesView
+                    .setLayoutParams(new GridView.LayoutParams(270, 270));
+
+        } else {
+            picturesView = (ImageView) convertView;
+        }
+
+        Glide.with(context).load(images.get(position))
+                .into(picturesView);
+
+        return picturesView;
+    }
+
+    /**
+     * Getting All Images Path.
+     *
+     * @param activity
+     *            the activity
+     * @return ArrayList with images Path
+     */
+    private ArrayList<String> getAllShownImagesPath(Activity activity) {
+        Uri uri;
+        Cursor cursor;
+        int column_index_data, column_index_folder_name;
+        ArrayList<String> listOfAllImages = new ArrayList<String>();
+        String absolutePathOfImage = null;
+        uri = android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+
+        String[] projection = { MediaStore.MediaColumns.DATA,
+                MediaStore.Images.Media.BUCKET_DISPLAY_NAME };
+
+        cursor = activity.getContentResolver().query(uri, projection, null,
+                null, null);
+
+        column_index_data = cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.DATA);
+        column_index_folder_name = cursor
+                .getColumnIndexOrThrow(MediaStore.Images.Media.BUCKET_DISPLAY_NAME);
+        while (cursor.moveToNext()) {
+            absolutePathOfImage = cursor.getString(column_index_data);
+
+            listOfAllImages.add(absolutePathOfImage);
+        }
+        return listOfAllImages;
+    }
 }
+
+
+//    /**
+//     * The Class ImageAdapter.
+//     */
+//    private class ImageAdapter extends BaseAdapter {
+//
+//        /** The context. */
+//        private Activity context;
+//
+//        /**
+//         * Instantiates a new image adapter.
+//         *
+//         * @param localContext
+//         *            the local context
+//         */
+//        public ImageAdapter(Activity localContext) {
+//            context = localContext;
+//            images = getAllShownImagesPath(context);
+//        }
+//
+//        public int getCount() {
+//            return images.size();
+//        }
+//
+//        public Object getItem(int position) {
+//            return position;
+//        }
+//
+//        public long getItemId(int position) {
+//            return position;
+//        }
+//
+//        public View getView(final int position, View convertView,
+//                            ViewGroup parent) {
+//            ImageView picturesView;
+//            if (convertView == null) {
+//                picturesView = new ImageView(context);
+//                picturesView.setScaleType(ImageView.ScaleType.FIT_CENTER);
+//                picturesView
+//                        .setLayoutParams(new GridView.LayoutParams(270, 270));
+//
+//            } else {
+//                picturesView = (ImageView) convertView;
+//            }
+//
+//            Glide.with(context).load(images.get(position))
+//                    .placeholder(R.drawable.ic_launcher).centerCrop()
+//                    .into(picturesView);
+//
+//            return picturesView;
+//        }
+//
+//        /**
+//         * Getting All Images Path.
+//         *
+//         * @param activity
+//         *            the activity
+//         * @return ArrayList with images Path
+//         */
+//        private ArrayList<String> getAllShownImagesPath(Activity activity) {
+//            Uri uri;
+//            Cursor cursor;
+//            int column_index_data, column_index_folder_name;
+//            ArrayList<String> listOfAllImages = new ArrayList<String>();
+//            String absolutePathOfImage = null;
+//            uri = android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+//
+//            String[] projection = { MediaColumns.DATA,
+//                    MediaStore.Images.Media.BUCKET_DISPLAY_NAME };
+//
+//            cursor = activity.getContentResolver().query(uri, projection, null,
+//                    null, null);
+//
+//            column_index_data = cursor.getColumnIndexOrThrow(MediaColumns.DATA);
+//            column_index_folder_name = cursor
+//                    .getColumnIndexOrThrow(MediaStore.Images.Media.BUCKET_DISPLAY_NAME);
+//            while (cursor.moveToNext()) {
+//                absolutePathOfImage = cursor.getString(column_index_data);
+//
+//                listOfAllImages.add(absolutePathOfImage);
+//            }
+//            return listOfAllImages;
+//        }
+//    }
+//}
